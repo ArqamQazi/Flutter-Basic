55031ebcab84961bfc905e1e1da4867df03e19da
ArqamQazi <128565091+ArqamQazi@users.noreply.github.com>
2023-11-10 20:52:17 +0530

    Container in flutter

---
+++ 1_container/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Container",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Container"),
+      ),
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          color: Colors.cyan,
+          child: const Center(
+            child: Text("Helloooooo"),
+          ),
+        ),
+      ),
+    );
+  }
+}
